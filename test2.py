bb6e948a86cc561fb67929acec81238ee0bc4a95
dlxortmd987 <dlxortmd987@gmail.com>
2023-02-20 14:54:39 +0900

    2

--- test2.py
+++ test2.py
@@ -6,21 +6,23 @@ from selenium.webdriver.common.keys import Keys
 
 from bs4 import BeautifulSoup
 
+STATIONS_SELECTOR = '#container > shrinkable-layout > div > subway-layout > subway-home-layout > div.sub > subway-directions-details > div > div > ul > li.item_route.type_subway1.ng-star-inserted > ul > li:nth-child'
+
+DETAIL_VIEW_XPATH = '//*[@id="container"]/shrinkable-layout/div/subway-layout/subway-home-layout/div[1]/div/subway-directions-list/div/div[1]/div[2]/button'
+
 ROUTE_SELECTOR = '#container > shrinkable-layout > div > subway-layout > subway-home-layout > div.sub > subway-directions-details > div > div > ul'
 
-REQUIRED_TIME_SELECTOR = '#container > shrinkable-layout > div > subway-layout > subway-home-layout > div.sub > subway-directions-details > div > div > div > div > strong'
+REQUIRED_TIME_SELECTOR = "#container > shrinkable-layout > div > subway-layout > subway-home-layout > div.main > div > subway-directions-list > div > div.direction_summary_area.active.ng-star-inserted > div.summary_area > div > strong"
 
 ROUTE_XPATH = '//*[@id="container"]/shrinkable-layout/div/subway-layout/subway-home-layout/div[2]/subway-directions-details/div/div/ul'
 
 REQUIRED_TIME_XPATH = '//*[@id="container"]/shrinkable-layout/div/subway-layout/subway-home-layout/div[2]/subway-directions-details/div/div/div/div/strong'
 
-DETAIL_VIEW_LINK_XPATH = '//*[@id="container"]/shrinkable-layout/div/subway-layout/subway-home-layout/div[1]/div/subway-directions-list/div/div[1]/div[2]/button'
-
 END_STATION_BUTTON_XPATH = '//*[@id="container"]/shrinkable-layout/div/subway-layout/subway-home-layout/div[1]/subway-control-panel/div/subway-input-control/div[1]/ul/li[2]/subway-input-control-item/div/subway-search-list/div/ul/li[1]/a'
 
 START_STATION_BUTTON_XPATH = '//*[@id="container"]/shrinkable-layout/div/subway-layout/subway-home-layout/div[1]/subway-control-panel/div/subway-input-control/div[1]/ul/li[1]/subway-input-control-item/div/subway-search-list/div/ul/li/a'
 
-STATION_INPUT_DELAY = 0.5
+STATION_INPUT_DELAY = 1
 
 DIRECTION_SEC = 0.5
 
@@ -35,15 +37,18 @@ assert '지하철 - 네이버 지도' in driver.title
 driver.find_element(By.ID, 'input_search_0').send_keys(start)
 time.sleep(STATION_INPUT_DELAY)
 driver.find_element(By.XPATH, START_STATION_BUTTON_XPATH).send_keys(Keys.ENTER)
-time.sleep(1)
 
+driver.find_element(By.ID, 'input_search_1').click()
 driver.find_element(By.ID, 'input_search_1').send_keys(end)
 time.sleep(STATION_INPUT_DELAY)
 driver.find_element(By.XPATH, END_STATION_BUTTON_XPATH).send_keys(Keys.ENTER)
 time.sleep(2)
 
+driver.find_element(By.XPATH, DETAIL_VIEW_XPATH).send_keys(Keys.ENTER)
+
 source = driver.page_source
 soup = BeautifulSoup(source, 'html.parser')
+
 
 required_time = soup.select_one(REQUIRED_TIME_SELECTOR).text.strip('분')
 print("소요 시간 ", required_time)
@@ -52,13 +57,3 @@ route = soup.select_one(ROUTE_SELECTOR)
 route_page = route.prettify()
 
 print(route_page)
-
-# time.sleep(0.5)
-# driver.find_element(By.XPATH, DETAIL_VIEW_LINK_XPATH).click()
-#
-#
-#
-# requiredTime = driver.find_element(By.XPATH, REQUIRED_TIME_XPATH).text.replace('분', '')
-# print("소요 시간 ", requiredTime)
-
-
